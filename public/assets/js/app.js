a7135ed396f84dd05ee96597a8181e5dd734ef0b
Your Name201~ <abolore200@gmail.com>
2023-04-03 12:44:29 +0100

    updated

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,4 +3,9 @@ const menuBtn = document.querySelector('.download-button .shareAllThings-menubar
 menuBtn.addEventListener('click', () => {
     const navMenu = document.querySelector('.sub-menu-fh-ds ul')
     navMenu.classList.toggle('showNavMenu')
+    if(navMenu.classList.contains('showNavMenu')){
+        document.querySelector('body').style.position = 'fixed'
+    } else {
+        document.querySelector('body').style.removeProperty('position')
+    }
 })
