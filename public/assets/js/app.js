524a94d3ddbfebc8b58238abf0995869df3aa03b
Your Name201~ <abolore200@gmail.com>
2023-04-03 14:15:19 +0100

    upadted

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,11 +1,12 @@
 const menuBtn = document.querySelector('.download-button .shareAllThings-menubar button')
-
 menuBtn.addEventListener('click', () => {
     const navMenu = document.querySelector('.sub-menu-fh-ds ul')
     navMenu.classList.toggle('showNavMenu')
     if(navMenu.classList.contains('showNavMenu')){
         document.querySelector('body').style.position = 'fixed'
+        document.querySelectorAll('input').forEach(input => {input.disabled = true})
     } else {
         document.querySelector('body').style.removeProperty('position')
+        document.querySelectorAll('input').forEach(input => {input.disabled = false})
     }
 })
