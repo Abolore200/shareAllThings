4f1e5ebd9a5d24a2efad77640b706dcb95239411
Your Name201~ <abolore200@gmail.com>
2023-04-05 14:09:45 +0100

    updated

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -13,3 +13,46 @@ menuBtn.addEventListener('click', () => {
 //add updated year
 const year = new Date().getFullYear()
 document.querySelector('.hl-pl-uf p span').innerHTML = year
+//if newsletter email input is empty, show error message
+const subscribeNewsletter = document.querySelector('.subscribe-for-newsletter')
+subscribeNewsletter.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const email = document.querySelector('.subscribe-for-newsletter #email').value
+    if(email === ''){
+        html.displayNewsError('email cannot be empty')
+    } else {
+        window.location.href = 'index.html'
+    }
+})
+document.querySelector('.registerEmail').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const email = document.querySelector('.registerEmail #email').value
+    if(email === ''){
+        html.displaySignUpError('email cannot be empty')
+    } else {
+        window.location.href = 'index.html'
+    }
+})
+class htmlui{
+    displayNewsError(error) {
+        const errorMsg = document.createElement('div')
+        errorMsg.classList.add('errorMsg')
+        errorMsg.innerHTML = `<p>${error}</p>`
+        //insert the div to errorMsg
+        subscribeNewsletter.insertBefore(errorMsg, document.querySelector('.subscribe-for-newsletter .input-field'))
+        setTimeout(() => {
+            errorMsg.remove()
+        },1500)
+    }
+    displaySignUpError(error) {
+        const errorMsg = document.createElement('div')
+        errorMsg.classList.add('errorMsg')
+        errorMsg.innerHTML = `<p>${error}</p>`
+        //insert the div to errorMsg
+        document.querySelector('.registerEmail').insertBefore(errorMsg, document.querySelector('.registerEmail .input-field'))
+        setTimeout(() => {
+            errorMsg.remove()
+        },1500)
+    }
+}
+const html = new htmlui()
